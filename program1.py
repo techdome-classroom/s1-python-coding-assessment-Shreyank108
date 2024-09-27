a31934b664b3919a35f10e33702cd125435274db
Shreyank Agrawal <122221017+Shreyank108@users.noreply.github.com>
2024-09-27 12:25:45 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -13,5 +13,14 @@ class Solution:
         
             for dr,dc in directions:  
                 nr,nc= r+dr, c+dc    
-                if 0<= nr < rows and 0<=nc<cols and grid[nr][nc]=='L'         
+                if 0<= nr < rows and 0<=nc<cols and grid[nr][nc]=='L': 
+                    dfs(nr,nc) 
+                    
+        num_islands=0  
+        
+        for r in range(rows): 
+            for c in range(cols): 
+                if grid[r][c] =='L': 
+                    dfs(r,c) 
+                             
         
