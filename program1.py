3a868c18f4e659e743d43ce1704f0222ac3a317a
Shreyank Agrawal <122221017+Shreyank108@users.noreply.github.com>
2024-09-27 12:22:57 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,6 +2,9 @@ class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
-         if not grid or not grid[0]: 
-                        
-        return 0
+        if not grid or not grid[0]: 
+            return 0 
+        
+        rows,cols = len(grid),len(grid[0]) 
+        direction = [(0,1),(1,0),(0,-1),(-1,0)]
+        
